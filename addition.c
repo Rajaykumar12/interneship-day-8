e2653bc6eddf266143ec04829085d10cd6f3befe
r-ajay-kumar <rajaykumar12959@gmail.com>
2024-09-11 15:28:49 +0530

    add: addition

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    int a,b,add=0;
+    printf("enter a\n");
+    scanf("%d",&a);
+    printf("enter b\n");
+    scanf("%d",&b);
+    add = a+b;
+    printf("Result: %d",add);
+}
